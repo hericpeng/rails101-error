0555fbf62e27169346003171de2732d5c8c1ba45
谭宇 <tanyucn@outlook.com>
2017-04-12 16:09:08 +0800

    error project

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412061958) do
+ActiveRecord::Schema.define(version: 20170412070746) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "desscription"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
